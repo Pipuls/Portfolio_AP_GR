d99c04889e9f855614cfd4d75d97e2198e4c9bb4
Pipuls <56545116+Pipuls@users.noreply.github.com>
2022-08-22 09:55:36 -0300

    creacion de login y correcciones de diseño,  guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/auth/login.component';
+import { RegisterComponent } from './components/auth/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { GuardsService as guard} from './services/security/guards.service';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: '**', redirectTo: '', pathMatch: 'full'},
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
